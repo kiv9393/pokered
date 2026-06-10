834ee7d373c9e7d32a2b7623cb3866194a949886
kiv9393 <kiv9393@gmail.com>
2026-06-10 21:57:09 +0000

    rumble project progress - MBC5 + injection attempts

---
+++ rumble_debug.lua
@@ -0,0 +1,21 @@
+local rumbleActive = false
+local frameCount = 0
+
+callbacks:add("frame", function()
+    frameCount = frameCount + 1
+    local val = emu:read8(0xC700)
+
+    if frameCount % 120 == 0 then
+        console:log("Heartbeat - Frame " .. frameCount .. " | C700 = " .. val)
+    end
+
+    if val == 1 and not rumbleActive then
+        rumbleActive = true
+        console:log("[Frame " .. frameCount .. "] >>> RUMBLE ON <<<")
+    elseif val == 0 and rumbleActive then
+        rumbleActive = false
+        console:log("[Frame " .. frameCount .. "] --- rumble off ---")
+    end
+end)
+
+console:log("Rumble debug v2 loaded! Watching C700 every 2 seconds...")
